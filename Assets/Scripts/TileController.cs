cb8f152c2a223ab1ae7678a0c5f5588b498d4b3f
Patrik S <absolut.patrik@hotmail.com>
2018-08-21 11:32:26 +0200

    tile switch working

---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour {
+
+    public bool empty = false;
+    public TileManager tileManager;
+
+
+    private void OnMouseDown()
+    {
+        //Debug.Log("Clickyclick on " + gameObject.name);
+        tileManager.TilePressed(this);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
